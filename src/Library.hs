4d3e8ec9777cb9e9d3dc00b9408d81fe0613d940
Facundo Agustín Fernández <facuassain@gmail.com>
2021-06-06 13:39:26 -0300

    punto 3 terminado

--- src/Library.hs
+++ src/Library.hs
@@ -30,9 +30,15 @@ rejuvenecer :: Raton -> Raton
 rejuvenecer raton = raton{edad = sqrt (edad raton) }
 
 hierbaVerde :: String -> Hierba
-hierbaVerde = eliminarEnfermedades
+hierbaVerde = eliminarEnfermedadesPor
 
-alcachofa :: Raton -> Raton
+hierbaZort :: Hierba
+hierbaZort = transformarEnPinky
+
+hierbaDelDiablo :: Hierba
+hierbaDelDiablo = eliminarEnfermedadesMenosDe10 . perderPeso
+
+alcachofa :: Hierba
 alcachofa raton
     | peso raton > 2 = perderPesoEn 0.1 raton
     | otherwise  = perderPesoEn 0.05 raton
@@ -43,14 +49,42 @@ perderPesoEn porcentaje raton = raton{peso = modificarPesoEn (-porcentaje * peso
 modificarPesoEn :: Number -> Raton -> Number
 modificarPesoEn cantidad = (+cantidad) . peso
 
-eliminarEnfermedades :: String -> Raton -> Raton
-eliminarEnfermedades terminacion raton = raton{enfermedades = filtrarEnfermedadesPor (not . terminaEn terminacion) raton}
+perderPeso :: Raton -> Raton
+perderPeso raton = raton{ peso = modificarPesoEn (min 0 (-0.1)) raton}
+
+eliminarEnfermedadesPor :: String -> Raton -> Raton
+eliminarEnfermedadesPor terminacion raton = raton{enfermedades = filtrarEnfermedadesPor (not . terminaEn terminacion) raton}
+
+eliminarEnfermedadesMenosDe10 :: Raton -> Raton
+eliminarEnfermedadesMenosDe10 raton = raton{enfermedades=filtrarEnfermedadesPor ((>=10).length) raton}
 
 filtrarEnfermedadesPor :: (String -> Bool) -> Raton -> [Enfermedad]
 filtrarEnfermedadesPor condicion raton = filter condicion (enfermedades raton)
 
 terminaEn :: String -> Enfermedad -> Bool 
-terminaEn terminacion = (==terminacion) . take (length terminacion) . reverse
+terminaEn terminacion = (==terminacion) . reverse . take (length terminacion) . reverse
+
+transformarEnPinky :: Raton -> Raton
+transformarEnPinky raton = raton{enfermedades=[],edad=0}
 
 -- PUNTO 3
--- type Medicamento = [Hierba]
+type Medicamento = Raton -> Raton
+type Potencia = Number 
+
+pondsAntiAge :: Medicamento
+pondsAntiAge = administrarHierbas [hierbaBuena,hierbaBuena,hierbaBuena,alcachofa]
+
+reduceFatFast :: Potencia -> Medicamento
+reduceFatFast potencia = administrarHierbas (replicate potencia alcachofa ++  [hierbaVerde "obesidad"])
+
+pdepCilina :: Medicamento
+pdepCilina = curarConHierbasVerdes
+
+curarConHierbasVerdes :: Raton -> Raton
+curarConHierbasVerdes raton = foldl (flip hierbaVerde) raton sufijosInfecciosas 
+
+administrarHierbas :: [Hierba] -> Raton -> Raton
+administrarHierbas hierbas raton = foldl (\raton hierba -> hierba raton) raton hierbas
+
+sufijosInfecciosas :: [String]
+sufijosInfecciosas = [ "sis", "itis", "emia", "cocos"]
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,13 +5,32 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Pruebas sobre las hierbas" $ do
-    it "la hierba buena rejuvenece a un raton" $ do
-        edad (hierbaBuena cerebro) `shouldBe` 3
-    it "la hierba verde elimina las enfermedades con cierta terminacion" $ do
-        enfermedades (hierbaVerde "sis" cerebro) `shouldBe` ["sarampion"]
-    it "las alcachofas hacen perder peso a un raton de 2 kg o menos" $ do
-        peso (alcachofa cerebro) `shouldBe` 0.19
-    it "las alcachofas hacen perder peso a un raton de mas de 2kg" $ do
-        peso (alcachofa huesudo) `shouldBe` 9
-
+    describe "Pruebas sobre las hierbas" $ do
+        it "la hierba buena rejuvenece a un raton" $ do
+            edad (hierbaBuena cerebro) `shouldBe` 3
+        it "la hierba verde elimina las enfermedades con cierta terminacion" $ do
+            enfermedades (hierbaVerde "sis" cerebro) `shouldBe` ["sarampion"]
+        it "las alcachofas hacen perder peso a un raton de 2 kg o menos" $ do
+            peso (alcachofa cerebro) `shouldBe` 0.19
+        it "las alcachofas hacen perder peso a un raton de mas de 2kg" $ do
+            peso (alcachofa huesudo) `shouldBe` 9
+        it "la hierba zort deja sin enfermedades al raton" $ do
+            length (enfermedades (hierbaZort cerebro)) `shouldBe` 0
+        it "la hierba zort deja con 0 años de edad al raton" $ do
+            edad (hierbaZort cerebro) `shouldBe` 0
+        it "la hierba del diablo hacer perder 0.1kg de peso" $ do
+            peso (hierbaDelDiablo cerebro) `shouldBe` 0.1
+        it "la hierba del diablo elimina las enfermedades con menos de 10 letras" $ do
+            enfermedades (hierbaDelDiablo cerebro) `shouldBe` ["brucelosis","tuberculosis"]
+    
+    describe "Pruebas sobre los medicamentos" $ do
+        it "el medicamento pondsAntiAge modifica la edad del raton" $ do
+            edad (pondsAntiAge bicenterrata) `shouldBe` 2
+        it "el medicamento pondsAntiAge modifica el peso del raton" $ do
+            peso (pondsAntiAge bicenterrata) `shouldBe` 0.19
+        it "el medicamento reduceFatFast modifica el peso de un raton" $ do
+            peso (reduceFatFast 2 huesudo) `shouldBe` 8.1
+        it "el medicamento reduceFatFast modifica las enfermedades del raton" $ do
+            enfermedades (reduceFatFast 2 huesudo) `shouldBe` ["sinusitis"]
+        it "el medicamento pdepCilina cura todas las enfermedades infecciosas de un raton" $ do
+            enfermedades (pdepCilina cerebro) `shouldBe` ["sarampion"]

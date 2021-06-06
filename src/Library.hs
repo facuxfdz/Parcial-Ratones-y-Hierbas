153c629b8d3582edae2599cb2478f69222d328f1
Facundo Agustín Fernández <facuassain@gmail.com>
2021-06-06 02:30:40 -0300

    alcachofas listas

--- src/Library.hs
+++ src/Library.hs
@@ -20,6 +20,7 @@ bicenterrata = UnRaton "bicenterrata" 256 0.2 []
 huesudo :: Raton
 huesudo = UnRaton "huesudo" 4 10 ["alta obesidad","sinusitis"]
 
+
 -- PUNTO 2
 type Hierba = Raton -> Raton
 hierbaBuena :: Hierba
@@ -31,10 +32,25 @@ rejuvenecer raton = raton{edad = sqrt (edad raton) }
 hierbaVerde :: String -> Hierba
 hierbaVerde = eliminarEnfermedades
 
+alcachofa :: Raton -> Raton
+alcachofa raton
+    | peso raton > 2 = perderPesoEn 0.1 raton
+    | otherwise  = perderPesoEn 0.05 raton
+
+perderPesoEn :: Number -> Raton -> Raton
+perderPesoEn porcentaje raton = raton{peso = modificarPesoEn (-porcentaje * peso raton) raton}
+
+modificarPesoEn :: Number -> Raton -> Number
+modificarPesoEn cantidad = (+cantidad) . peso
+
 eliminarEnfermedades :: String -> Raton -> Raton
-eliminarEnfermedades terminacion raton = raton{enfermedades=filter (not . terminaEn terminacion) (enfermedades raton)}
+eliminarEnfermedades terminacion raton = raton{enfermedades = filtrarEnfermedadesPor (not . terminaEn terminacion) raton}
+
+filtrarEnfermedadesPor :: (String -> Bool) -> Raton -> [Enfermedad]
+filtrarEnfermedadesPor condicion raton = filter condicion (enfermedades raton)
 
 terminaEn :: String -> Enfermedad -> Bool 
-terminaEn terminacion enfermedad = take (length terminacion) (reverse enfermedad) == terminacion 
+terminaEn terminacion = (==terminacion) . take (length terminacion) . reverse
+
 -- PUNTO 3
 -- type Medicamento = [Hierba]
--- src/Spec.hs
+++ src/Spec.hs
@@ -10,4 +10,8 @@ correrTests = hspec $ do
         edad (hierbaBuena cerebro) `shouldBe` 3
     it "la hierba verde elimina las enfermedades con cierta terminacion" $ do
         enfermedades (hierbaVerde "sis" cerebro) `shouldBe` ["sarampion"]
+    it "las alcachofas hacen perder peso a un raton de 2 kg o menos" $ do
+        peso (alcachofa cerebro) `shouldBe` 0.19
+    it "las alcachofas hacen perder peso a un raton de mas de 2kg" $ do
+        peso (alcachofa huesudo) `shouldBe` 9
 
